f31e17d64a6a933672ab466dd984530638bf8a04
Rallefar1103 <rasmus@turftank.com>
2023-12-21 01:14:56 -0800

    lets the graphql itinerary type adhere to mongoDB schema

--- src/schema/types/itinerary.js
+++ src/schema/types/itinerary.js
@@ -1,20 +1,56 @@
 const { gql } = require("apollo-server-express");
 
 const itineraryType = gql`
+  type DiningOptions {
+    type: String
+    cuisine: String
+    priceRange: String
+  }
+
+  type AttractionOptions {
+    type: [String]
+  }
+
+  type UserPreferences {
+    diningOptions: DiningOptions
+    attractionOptions: AttractionOptions
+  }
+
   type Itinerary {
     id: ID!
-    title: String!
-    description: String!
-    recommendations: String
+    title: String
+    destination: String
+    duration: String
+    budget: String
+    userPreferences: UserPreferences
+    recommendedItineraryDescription: String
+  }
+
+  input DiningOptionsInput {
+    type: String
+    cuisine: String
+    priceRange: String
+  }
+
+  input AttractionOptionsInput {
+    type: [String]
+  }
+
+  input UserPreferencesInput {
+    diningOptions: DiningOptionsInput
+    attractionOptions: AttractionOptionsInput
+  }
+
+  input ItineraryInput {
+    title: String
+    destination: String
+    duration: String
+    budget: String
+    userPreferences: UserPreferencesInput
   }
 
   type Query {
     getItinerary(id: ID!): Itinerary
-  }
-
-  input ItineraryInput {
-    title: String!
-    description: String!
   }
 
   type Mutation {
